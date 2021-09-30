145a124cb64127f5d5ecb2e13c63adde11b0ed2d
rwagnervm <rwagnervm@gmail.com>
2021-09-30 13:39:23 -0300

    Inicial

---
+++ build/precache-manifest.f0ca0e0feb859c32ead83d1ad0f2129f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7f3f75d1c028c035ca9c311d490e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "197b67f8f18747ea92b5",
+    "url": "/static/js/2.b30fd5a1.chunk.js"
+  },
+  {
+    "revision": "cd22f0735adee52721471850216b8b31",
+    "url": "/static/js/2.b30fd5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f89097ffa670870838",
+    "url": "/static/js/main.8f27fda7.chunk.js"
+  },
+  {
+    "revision": "ee04820d50e6f0d7df74",
+    "url": "/static/js/runtime-main.30a96da5.js"
+  }
+]);
